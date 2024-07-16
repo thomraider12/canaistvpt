6950188bf648c5399bd476cd8fc7ebe3f1056c8d
thomraider12 <168651587+thomraider12@users.noreply.github.com>
2024-07-16 12:12:08 +0100

    Update streams.sh

--- streams.sh
+++ streams.sh
@@ -4,14 +4,10 @@ cd M3U
 
 # TVI - update the stream URL of TVI
 
-sed -i "/live_tvi\/live_tvi/ c https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" M3UPT.m3u
+sed -i "/live_tvi\/live_tvi/ c https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" pt.m3u
 
 # CNN Portugal - update the stream URL of CNN Portugal
 
-sed -i "/live_cnn/ c https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" M3UPT.m3u
-
-# TVI Internacional - update the stream URL of TVI Internacional
-
-sed -i "/live_tvi_internacional/
+sed -i "/live_cnn/ c https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" pt.m3u
 
 exit 0
