75eb6a43b0f6210ade051a9d106d0ac9b07c9acf
thomraider12 <168651587+thomraider12@users.noreply.github.com>
2024-07-16 12:15:20 +0100

    Update streams.sh

--- streams.sh
+++ streams.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-cd M3U
-
 # TVI - update the stream URL of TVI
 
 sed -i "/live_tvi\/live_tvi/ c https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" pt.m3u
