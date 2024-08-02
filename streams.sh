3ce3cb4a48e5a3a0bf824c32c12af8f062077f95
thomraider12 <168651587+thomraider12@users.noreply.github.com>
2024-08-02 09:28:52 +0100

    Update streams.sh

--- streams.sh
+++ streams.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# TVI - update the stream URL of TVI
+# TVI - atualizar o URL da TVI
 sed -i "/live_tvi\/live_tvi/ c https://video-auth6.iol.pt/live_tvi/live_tvi/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" pt.m3u
 if grep -q "live_tvi\/live_tvi" pt.m3u; then
-  echo "TVI URL updated successfully"
+  echo "Link da TVI atualizado."
 else
-  echo "Failed to update TVI URL"
+  echo "Link da TVI não atualizado."
 fi
 
-# CNN Portugal - update the stream URL of CNN Portugal
+# CNN Portugal - atualizar o URL da CNN Portugal
 sed -i "/live_cnn/ c https://video-auth7.iol.pt/live_cnn/live_cnn/playlist.m3u8?wmsAuthSign=$(wget https://services.iol.pt/matrix?userId= -o /dev/null -O -)/" pt.m3u
 if grep -q "live_cnn" pt.m3u; then
-  echo "CNN URL updated successfully"
+  echo "Link da CNN Portugal atualizado com sucesso."
 else
-  echo "Failed to update CNN URL"
+  echo "Link da CNN Portugal não atualizado."
 fi
 
 exit 0
