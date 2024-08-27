4b0a8df90b6cb4a9d20065a84ff11246865b409f
thomraider12 <168651587+thomraider12@users.noreply.github.com>
2024-08-27 18:22:31 +0100

    Update epg-pt.sh

--- EPG/epg-pt.sh
+++ EPG/epg-pt.sh
@@ -6,30 +6,51 @@ cd /home/runner/work/canaistvpt/canaistvpt/iptv-org-epg && npm install
 
 npm run grab -- --channels=../EPG/pt.channels.xml --output=../EPG/epg-pt.xml --days=7 --maxConnections=20
 
-# Meo EPG
+echo -e "\n\nEPG PT atualizada!\n"
+sleep 3
+
+# MEO EPG
 
 npm run grab -- --channels=../EPG/meo.pt.channels.xml --output=../EPG/epg-meo-pt.xml --days=7 --maxConnections=20
 
-# Nos EPG
+echo -e "\n\nEPG da MEO atualizada!\n"
+sleep 3
+
+# NOS EPG
 
 npm run grab -- --channels=../EPG/nos.pt.channels.xml --output=../EPG/epg-nos-pt.xml --days=7 --maxConnections=20
+
+echo -e "\n\nEPG da NOS atualizada!\n"
+sleep 3
 
 # RTP EPG
 
 npm run grab -- --site=rtp.pt --output=../EPG/epg-rtp-pt.xml --days=7 --maxConnections=20
 
+echo -e "\n\nEPG da RTP atualizada!\n"
+sleep 3
+
 # Rytec EPG
 
 cd ../EPG
-
 wget -O epg-rytec-pt.xml.xz "http://www.xmltvepg.nl/rytecPT.xz"
 
-# Compress EPG xml files
+echo -e "\n\nEPG da Rytec atualizada!\n"
+sleep 3
+
+# Comprimir ficheiros XML da EPG
 
 xz -k -f -9 epg*.xml && gzip -k -f -9 epg*.xml
 
-# Remove EPG xml files
+echo -e "\n\nFicheiros comprimidos.\n"
+sleep 3
+
+# Apagar ficheiros XML
 
 rm epg*.xml
+
+echo -e "\n\nFicheiros apagados!\nA sair!"
+sleep 3
+
 
 exit 0
