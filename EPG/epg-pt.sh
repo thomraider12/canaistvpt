f143368fd01ba240049b7aab5d1077ceaac05c6f
thomraider12 <168651587+thomraider12@users.noreply.github.com>
2024-08-28 10:49:14 +0100

    Update epg-pt.sh

--- EPG/epg-pt.sh
+++ EPG/epg-pt.sh
@@ -4,28 +4,28 @@ cd /home/runner/work/canaistvpt/canaistvpt/iptv-org-epg && npm install
 
 # PT EPG
 
-npm run grab -- --channels=../EPG/pt.channels.xml --output=../EPG/epg-pt.xml --days=7 --maxConnections=20
+npm run grab -- --channels=../EPG/pt.channels.xml --output=../EPG/epg-pt.xml --days=7 --maxConnections=50
 
 echo "EPG PT atualizada!"
 sleep 3
 
 # MEO EPG
 
-npm run grab -- --channels=../EPG/meo.pt.channels.xml --output=../EPG/epg-meo-pt.xml --days=7 --maxConnections=20
+npm run grab -- --channels=../EPG/meo.pt.channels.xml --output=../EPG/epg-meo-pt.xml --days=7 --maxConnections=50
 
 echo "EPG da MEO atualizada!"
 sleep 3
 
 # NOS EPG
 
-npm run grab -- --channels=../EPG/nos.pt.channels.xml --output=../EPG/epg-nos-pt.xml --days=7 --maxConnections=20
+npm run grab -- --channels=../EPG/nos.pt.channels.xml --output=../EPG/epg-nos-pt.xml --days=7 --maxConnections=50
 
 echo "EPG da NOS atualizada!"
 sleep 3
 
 # RTP EPG
 
-npm run grab -- --site=rtp.pt --output=../EPG/epg-rtp-pt.xml --days=7 --maxConnections=20
+npm run grab -- --site=rtp.pt --output=../EPG/epg-rtp-pt.xml --days=7 --maxConnections=50
 
 echo "EPG da RTP atualizada!"
 sleep 3
