a456db856b107ab855e8dfde70cece3e86ea2234
thomraider12 <168651587+thomraider12@users.noreply.github.com>
2024-10-04 20:55:31 +0100

    Update opto.py

--- EPG/scripts/opto.py
+++ EPG/scripts/opto.py
@@ -2,8 +2,7 @@ import requests
 import json
 import os
 import time
-import lzma
-import xmltodict
+import lzma  # Importando a biblioteca para compressão
 
 # Obter a data atual e a data de 2 dias depois em formato Unix
 start_date = int(time.time())
@@ -20,36 +19,23 @@ if response.status_code == 200:
     # Obtém o conteúdo em JSON
     epg_data = response.json()
     
-    # Caminho absoluto para a pasta EPG
-    epg_directory = os.path.join(os.path.dirname(__file__), '..', 'EPG')
-
-    # Cria a pasta EPG caso não exista
-    os.makedirs(epg_directory, exist_ok=True)
+    # Caminho do arquivo JSON
+    json_file_path = os.path.join("..", "EPG", "epg-sic-pt.json")
     
-    # Caminho do arquivo XML
-    xml_file_path = os.path.join(epg_directory, "epg-sic-pt.xml")
+    # Salva o JSON em um arquivo
+    with open(json_file_path, 'w') as json_file:
+        json.dump(epg_data, json_file, indent=4)
     
-    # Encapsula o conteúdo JSON em uma raiz chamada "programs" (ou qualquer outro nome)
-    epg_root = {"programs": epg_data}
-    
-    # Converte o JSON encapsulado para XML
-    xml_data = xmltodict.unparse(epg_root, pretty=True)  # Converte para XML
-    
-    # Salva o XML em um arquivo
-    with open(xml_file_path, 'w') as xml_file:
-        xml_file.write(xml_data)
-
-    print(f"EPG salva em: {xml_file_path}")
+    print(f"EPG salva em: {json_file_path}")
 
     # Caminho do arquivo .xz
-    xz_file_path = os.path.join(epg_directory, "epg-sic-pt.xml.xz")
+    xz_file_path = os.path.join("EPG", "epg-sic-pt.json.xz")
 
-    # Compacta o arquivo XML em .xz
-    with open(xml_file_path, 'rb') as xml_file:
+    # Compacta o arquivo JSON em .xz
+    with open(json_file_path, 'rb') as json_file:
         with lzma.open(xz_file_path, 'wb') as xz_file:
-            xz_file.write(xml_file.read())
+            xz_file.write(json_file.read())
 
     print(f"EPG comprimida em: {xz_file_path}")
-
 else:
     print(f"Erro ao obter EPG: {response.status_code} - {response.text}")
