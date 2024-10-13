93d4f3b96128e82e2a3c4a64fd8ff044bcc0cdc0
thomraider12 <168651587+thomraider12@users.noreply.github.com>
2024-10-13 16:55:40 +0100

    Update extra.py

--- EPG/scripts/extra.py
+++ EPG/scripts/extra.py
@@ -4,11 +4,11 @@ from datetime import datetime, timedelta
 canais_extras = [
     {"id": "GoloFM.pt", "nome": "Golo FM - Desporto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/golofm.png", "desc": "A Primeira Rádio Desporto.\nA maior fábrica de notícias de Portugal que pode ouvir em FM e online."},
     {"id": "AviaçãoTV.pt", "nome": "AviaçãoTV - Direto (Lisboa)", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/aviaçãotv.png", "desc": "Assiste ao aeroporto de Lisboa em direto todos os dias."},
-    {"id": "OnFM.pt", "nome": "OnFM - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/onfm.png", "desc": "Liga-te.\nA on FM é a primeira visual rádio em Portugal."},
+    {"id": "OnFM.pt", "nome": "OnFM - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/onfm.png", "desc": "Liga-te.\nA OnFM é a primeira visual rádio em Portugal."},
     {"id": "DJTomasA", "nome": "DJ Tomás Afonso", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/tomasafonso.ico", "desc": "Ouve o DJ Tomás Afonso."},
     {"id": "RadioComercial.pt", "nome": "Rádio Comercial - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/radiocomercial.png", "desc": "A Melhor Música, sempre.\nEm casa, no carro em todo o lado."},
     {"id": "RFM.pt", "nome": "RFM - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/rfm.png", "desc": "Só grandes músicas."},
-    {"id": "FamaRadio.pt", "nome": "Fama Rádio - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/famaradio.png", "desc": "A Melhor Seleção de Todas.\nUma estação de Rádio FM com um forte posicionamento nos clássicos PopRock anos 80 90 e 00 alternativo às atuais ofertas deste segmento."},
+    {"id": "FamaRadio.pt", "nome": "Fama Rádio - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/famaradio.png", "desc": "A Melhor Seleção de Todas.\nUma estação de Rádio FM com um forte posicionamento nos clássicos; PopRock; anos 80 90 e 00, alternativo às atuais ofertas deste segmento."},
     {"id": "Observador.pt", "nome": "Observador - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/observador.png", "desc": "O Observador é um jornal diário online, independente e livre."},
     {"id": "TSF.pt", "nome": "TSF - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/tsf.png", "desc": "Vamos ao fim da rua, Vamos ao fim do mundo.\nThe reference for radio news in Portugal."},
     {"id": "M80.pt", "nome": "M80 - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/m80.png", "desc": "Se a sua vida tem uma música, ela passa na M80."},
@@ -21,7 +21,7 @@ canais_extras = [
     {"id": "CidadeFM.pt", "nome": "Cidade FM - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/cidadefm.png", "desc": "Só se quiseres\nA 1ª rádio dos exitos."},
     {"id": "CidadeHoje.pt", "nome": "Cidade Hoje - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/cidadehoje.png", "desc": "Rádio e Jornal de Vila Nova de Famalicão\nCidade Hoje Rádio"},
     {"id": "TugaFM.pt", "nome": "Tuga FM - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/tugafm.png", "desc": "Tuga FM."},
-    {"id": "TruckersFM.uk", "nome": "TruckersFM - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/truckersfm.png", "desc": "Your Drive, Your Music. Radio made for driving. Your destination for great music.\nOn Air since 2015, Truckers.FM is an radio station that serves drivers, and the simulation gaming community. Focused primary on playing chart."}
+    {"id": "TruckersFM.uk", "nome": "TruckersFM - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/truckersfm.png", "desc": "Your Drive, Your Music. Radio made for driving. Your destination for great music.\nOn Air since 2015, Truckers.FM is an radio station that serves drivers, and the simulation gaming community.\nFocused primary on playing chart."}
 ]
 
 # Criação do elemento raiz <tv>
