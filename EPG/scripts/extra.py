66c9638ce12694f4fca127f9735719b4e9e0156f
thomraider12 <168651587+thomraider12@users.noreply.github.com>
2024-10-13 16:44:42 +0100

    Update extra.py

--- EPG/scripts/extra.py
+++ EPG/scripts/extra.py
@@ -2,26 +2,26 @@ import xml.etree.ElementTree as ET
 from datetime import datetime, timedelta
 
 canais_extras = [
-    {"id": "GoloFM.pt", "nome": "Golo FM - Desporto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/golofm.png"},
-    {"id": "AviaçãoTV.pt", "nome": "AviaçãoTV - Direto (Lisboa)", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/aviaçãotv.png"},
-    {"id": "OnFM.pt", "nome": "OnFM - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/onfm.png"},
-    {"id": "DJTomasA", "nome": "DJ Tomás Afonso", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/tomasafonso.ico"},
-    {"id": "RadioComercial.pt", "nome": "Rádio Comercial - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/radiocomercial.png"},
-    {"id": "RFM.pt", "nome": "RFM - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/rfm.png"},
-    {"id": "FamaRadio.pt", "nome": "Fama Rádio - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/famaradio.png"},
-    {"id": "Observador.pt", "nome": "Observador - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/observador.png"},
-    {"id": "TSF.pt", "nome": "TSF - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/tsf.png"},
-    {"id": "M80.pt", "nome": "M80 - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/m80.png"},
-    {"id": "VozSantoTirso.pt", "nome": "Rádio Voz Santo Tirso - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/radiovozsantotirso.png"},
-    {"id": "MegaHits.pt", "nome": "MegaHits - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/megahits.png"},
-    {"id": "NovaEra.pt", "nome": "Nova Era - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/novaera.png"},
-    {"id": "MEOSudoeste.pt", "nome": "MEO Sudoeste - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/meosudoeste.png"},
-    {"id": "Orbital.pt", "nome": "Orbital - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/orbital.png"},
-    {"id": "Nove3Cinco.pt", "nome": "Nove3Cinco - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/nove3cinco.png"},
-    {"id": "CidadeFM.pt", "nome": "Cidade FM - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/cidadefm.png"},
-    {"id": "CidadeHoje.pt", "nome": "Cidade Hoje - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/cidadehoje.png"},
-    {"id": "TugaFM.pt", "nome": "Tuga FM - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/tugafm.png"},
-    {"id": "TruckersFM.uk", "nome": "TruckersFM - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/truckersfm.png"}
+    {"id": "GoloFM.pt", "nome": "Golo FM - Desporto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/golofm.png", "desc": "A Primeira Rádio Desporto.\nA maior fábrica de notícias de Portugal que pode ouvir em FM e online."},
+    {"id": "AviaçãoTV.pt", "nome": "AviaçãoTV - Direto (Lisboa)", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/aviaçãotv.png", "desc": "Assiste ao aeroporto de Lisboa em direto todos os dias."},
+    {"id": "OnFM.pt", "nome": "OnFM - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/onfm.png", "desc": "Liga-te.\nA on FM é a primeira visual rádio em Portugal."},
+    {"id": "DJTomasA", "nome": "DJ Tomás Afonso", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/tomasafonso.ico", "desc": "Ouve o DJ Tomás Afonso."},
+    {"id": "RadioComercial.pt", "nome": "Rádio Comercial - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/radiocomercial.png", "desc": "A Melhor Música, sempre.\nEm casa, no carro em todo o lado."},
+    {"id": "RFM.pt", "nome": "RFM - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/rfm.png", "desc": "Só grandes músicas."},
+    {"id": "FamaRadio.pt", "nome": "Fama Rádio - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/famaradio.png", "desc": "A Melhor Seleção de Todas.\nUma estação de Rádio FM com um forte posicionamento nos clássicos PopRock anos 80 90 e 00 alternativo às atuais ofertas deste segmento."},
+    {"id": "Observador.pt", "nome": "Observador - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/observador.png", "desc": "O Observador é um jornal diário online, independente e livre."},
+    {"id": "TSF.pt", "nome": "TSF - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/tsf.png", "desc": "Vamos ao fim da rua, Vamos ao fim do mundo.\nThe reference for radio news in Portugal."},
+    {"id": "M80.pt", "nome": "M80 - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/m80.png", "desc": "Se a sua vida tem uma música, ela passa na M80."},
+    {"id": "VozSantoTirso.pt", "nome": "Rádio Voz Santo Tirso - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/radiovozsantotirso.png", "desc": "Uma voz amiga\nA Rádio Voz de Santo Tirso é uma estação local, generalista com uma programação diversificada, cultural, entertenimento e musical de forma a integrar os valores artísticos locais, nacionais e estrangeiros."},
+    {"id": "MegaHits.pt", "nome": "MegaHits - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/megahits.png", "desc": "Mais Música Nova\n45 minutos de música sem parar"},
+    {"id": "NovaEra.pt", "nome": "Nova Era - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/novaera.png", "desc": "Música Sem Parar.\nAnimar, agitar e fazer vibrar os melhores ouvintes do Mundo."},
+    {"id": "MEOSudoeste.pt", "nome": "MEO Sudoeste - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/meosudoeste.png", "desc": "Um Festival de Rádio\nDedicated to festival Meo Sudoeste, promoted by Altice group and originally named Festival Sudoeste in portuguese southwest in The 1990 decade. Actually is also on radio station with various frequences in Portugal."},
+    {"id": "Orbital.pt", "nome": "Orbital - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/orbital.png", "desc": "A dar te música."},
+    {"id": "Nove3Cinco.pt", "nome": "Nove3Cinco - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/nove3cinco.png", "desc": "Esta é a Rádio Deejay.\nRadio from Póvoa de Lanhoso"},
+    {"id": "CidadeFM.pt", "nome": "Cidade FM - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/cidadefm.png", "desc": "Só se quiseres\nA 1ª rádio dos exitos."},
+    {"id": "CidadeHoje.pt", "nome": "Cidade Hoje - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/cidadehoje.png", "desc": "Rádio e Jornal de Vila Nova de Famalicão\nCidade Hoje Rádio"},
+    {"id": "TugaFM.pt", "nome": "Tuga FM - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/tugafm.png", "desc": "Tuga FM."},
+    {"id": "TruckersFM.uk", "nome": "TruckersFM - Direto", "logo": "https://raw.githubusercontent.com/thomraider12/icones_tv/main/truckersfm.png", "desc": "Your Drive, Your Music. Radio made for driving. Your destination for great music.\nOn Air since 2015, Truckers.FM is an radio station that serves drivers, and the simulation gaming community. Focused primary on playing chart."}
 ]
 
 # Criação do elemento raiz <tv>
@@ -56,7 +56,7 @@ for canais_extra in canais_extras:
 
         # Adiciona a descrição do programa
         description = ET.SubElement(programme, 'desc', lang="pt")
-        description.text = f"Programação contínua de {canais_extra['nome']}."
+        description.text = canais_extra["desc"]
 
         # Adiciona o ícone do programa
         icon = ET.SubElement(programme, 'icon', src=canais_extra["logo"])
