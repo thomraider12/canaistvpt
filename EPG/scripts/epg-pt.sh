bcb580f980a8cb3ce14e15cc997cf3bf03101ab2
thomraider12 <168651587+thomraider12@users.noreply.github.com>
2024-10-22 21:34:30 +0100

    Update epg-pt.sh

--- EPG/scripts/epg-pt.sh
+++ EPG/scripts/epg-pt.sh
@@ -1,41 +1,34 @@
 #!/bin/bash
 
+# Mover para a pasta iptv-org-epg onde o npm está instalado
 cd /home/runner/work/canaistvpt/canaistvpt/iptv-org-epg && npm install
 
 # PT EPG
-
 npm run grab -- --channels=../EPG/pt.channels.xml --output=../EPG/epg-pt.xml --days=7 --maxConnections=50
-
 echo "EPG PT atualizada!"
 sleep 3
 
 # MEO EPG
-
 npm run grab -- --channels=../EPG/meo.pt.channels.xml --output=../EPG/epg-meo-pt.xml --days=7 --maxConnections=50
-
 echo "EPG da MEO atualizada!"
 sleep 3
 
 # NOS EPG
-
 npm run grab -- --channels=../EPG/nos.pt.channels.xml --output=../EPG/epg-nos-pt.xml --days=7 --maxConnections=50
-
 echo "EPG da NOS atualizada!"
 sleep 3
 
-# Comprimir ficheiros XML da EPG
+# Mudar para a pasta EPG para comprimir e apagar os ficheiros
+cd ../EPG
 
-gzip -k -f -9 epg*.xml
-
-echo -e "Ficheiros comprimidos."
+# Comprimir ficheiros XML na pasta EPG
+gzip -f -9 epg*.xml
+echo "Ficheiros comprimidos."
 sleep 3
 
-# Apagar ficheiros XML
-
+# Apagar ficheiros XML na pasta EPG
 rm epg*.xml
-
-echo "Ficheiros apagados! A sair!"
+echo "Ficheiros XML apagados! A sair!"
 sleep 3
-
 
 exit 0
