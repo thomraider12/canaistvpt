4a72f0c62e1e86da1e2511ab9a946c8bfc32aa40
thomraider12 <168651587+thomraider12@users.noreply.github.com>
2024-11-23 11:53:05 +0000

    Update corrigir_nos.py

--- EPG/scripts/corrigir_nos.py
+++ EPG/scripts/corrigir_nos.py
@@ -31,7 +31,7 @@ def corrigir_xml(file_path):
 # Lista de arquivos EPG para corrigir
 epg_files = [
     'EPG/epg-pt.xml.gz'
-    'EPG/epg-test.xml.gz'
+    'EPG/epg-test-pt.xml.gz'
 ]
 
 # Corrigir e substituir todos os arquivos da lista
