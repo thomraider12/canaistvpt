1df758e1c103d6ace2f094d8f56a514fb7115926
Tomás Afonso <168651587+thomraider12@users.noreply.github.com>
2025-04-17 11:51:13 +0100

    Update epg-pt.sh

--- EPG/scripts/epg-pt.sh
+++ EPG/scripts/epg-pt.sh
@@ -2,17 +2,17 @@
 cd /home/runner/work/canaistvpt/canaistvpt/iptv-org-epg && npm install
 
 # PT EPG
-npm run grab -- --channels=../EPG/pt.channels.xml --output=../EPG/epg-pt.xml --days=8 --maxConnections=500
+npm run grab --- --channels=../EPG/pt.channels.xml --output=../EPG/epg-pt.xml --days=8 --maxConnections=500
 echo "EPG PT atualizada!"
 
 # NOS EPG
-npm run grab -- --channels=../EPG/meo.pt.channels.xml --output=../EPG/epg-meo-pt.xml --days=8 --maxConnections=500
+npm run grab --- --channels=../EPG/meo.pt.channels.xml --output=../EPG/epg-meo-pt.xml --days=8 --maxConnections=500
 echo "EPG da MEO atualizada!"
 
-npm run grab -- --channels=../EPG/testepg.pt.channels.xml --output=../EPG/epg-test-pt.xml --days=8 --maxConnections=800
+npm run grab --- --channels=../EPG/testepg.pt.channels.xml --output=../EPG/epg-test-pt.xml --days=8 --maxConnections=800
 echo "EPG de Teste atualizada!"
 
-npm run grab -- --channels=../EPG/opto.sic.pt.channels.xml --output=../EPG/epg-sic-pt.xml --days=8 --maxConnections=100
+npm run grab --- --channels=../EPG/opto.sic.pt.channels.xml --output=../EPG/epg-sic-pt.xml --days=8 --maxConnections=100
 echo "EPG da Opto atualizada!"
 
 # Mudar para a pasta EPG para comprimir e apagar os ficheiros
